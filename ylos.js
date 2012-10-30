9b4469bd16aba9058c69bfaa914f3f7cc6ec2488
ylos <fortega@ylos.com>
2012-10-30 12:12:30 +0100

    funcion resize agregada

---
+++ ylos.js
@@ -0,0 +1,27 @@
+    jQuery(window).load(function(){
+    jQuery('.resizecut img').each(function(){
+      var padre = jQuery('.resizecut');
+      var hijo = jQuery(this);
+      padre.css({position:'relative'});  
+        hijo.css({position:'absolute'});
+      var widthelement = hijo.width();
+      var heightelement = hijo.height();
+      var newwidth = padre.width();
+      var newheight = padre.height();
+        if( widthelement < heightelement ){
+    		newheight = heightelement / ( widthelement / newwidth );
+            hijo.css({
+    			width: newwidth,
+                top: (( padre.height() / 2 ) - ( newheight / 2 )) + 'px',
+    			left: '0px'
+            });
+        } 
+        else {
+            newwidth = widthelement / ( heightelement / newheight );
+    	    hijo.css({
+                height: newheight,
+                left: (( padre.width() / 2 ) - ( newwidth / 2 )) + 'px'
+            });
+        }
+    });
+    });
